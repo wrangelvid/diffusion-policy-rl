ea7baafdee0e6c26cfd1030f1fa6c2b2d211e2d8
David von Wrangel <49842806+wrangelvid@users.noreply.github.com>
2023-05-12 19:42:33 -0400

    Initial attempt in training the hybrid model (#5)

--- diffusion_policy/dataset/maniskill_dataset.py
+++ diffusion_policy/dataset/maniskill_dataset.py
@@ -7,7 +7,8 @@ from diffusion_policy.common.replay_buffer import ReplayBuffer
 from diffusion_policy.common.sampler import (SequenceSampler, get_val_mask,
                                              downsample_mask)
 from diffusion_policy.model.common.normalizer import LinearNormalizer
-from diffusion_policy.dataset.base_dataset import BaseLowdimDataset
+from diffusion_policy.dataset.base_dataset import BaseLowdimDataset, BaseImageDataset
+from diffusion_policy.common.normalize_util import get_image_range_normalizer
 
 
 class ManiDiffusionState(BaseLowdimDataset):
@@ -79,5 +80,82 @@ class ManiDiffusionState(BaseLowdimDataset):
         sample = self.sampler.sample_sequence(idx)
         data = self._sample_to_data(sample)
 
+        torch_data = dict_apply(data, torch.from_numpy)
+        return torch_data
+
+
+class ManiDiffusionRGBD(BaseImageDataset):
+
+    def __init__(self,
+                 zarr_path,
+                 horizon=1,
+                 pad_before=0,
+                 pad_after=0,
+                 seed=42,
+                 val_ratio=0.0,
+                 max_train_episodes=None):
+        super().__init__()
+        self.replay_buffer = ReplayBuffer.copy_from_path(
+            zarr_path,
+            keys=['rgbd', 'state', 'action'])
+
+        val_mask = get_val_mask(n_episodes=self.replay_buffer.n_episodes,
+                                val_ratio=val_ratio,
+                                seed=seed)
+        train_mask = ~val_mask
+        train_mask = downsample_mask(mask=train_mask,
+                                     max_n=max_train_episodes,
+                                     seed=seed)
+
+        self.sampler = SequenceSampler(replay_buffer=self.replay_buffer,
+                                       sequence_length=horizon,
+                                       pad_before=pad_before,
+                                       pad_after=pad_after,
+                                       episode_mask=train_mask)
+        self.train_mask = train_mask
+        self.horizon = horizon
+        self.pad_before = pad_before
+        self.pad_after = pad_after
+
+    def get_validation_dataset(self):
+        val_set = copy.copy(self)
+        val_set.sampler = SequenceSampler(replay_buffer=self.replay_buffer,
+                                          sequence_length=self.horizon,
+                                          pad_before=self.pad_before,
+                                          pad_after=self.pad_after,
+                                          episode_mask=~self.train_mask)
+        val_set.train_mask = ~self.train_mask
+        return val_set
+
+    def get_normalizer(self, mode='limits', **kwargs):
+        data = {
+            'action': self.replay_buffer['action'],
+            'rgbd': self.replay_buffer['rgbd'],
+            'state': self.replay_buffer['state'],
+        }
+        normalizer = LinearNormalizer()
+        normalizer.fit(data=data, last_n_dims=1, mode=mode, **kwargs)
+        normalizer['rgbd'] = get_image_range_normalizer()
+        return normalizer
+
+    def get_all_actions(self) -> torch.Tensor:
+        return torch.from_numpy(self.replay_buffer[self.action_key])
+
+    def __len__(self) -> int:
+        return len(self.sampler)
+
+    def _sample_to_data(self, sample):
+        data = {
+            'obs': {
+                'rgbd': sample['rgbd'],
+                'state': sample['state'],
+            },
+            'action': sample['action'],  # T, D_a
+        }
+        return data
+
+    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
+        sample = self.sampler.sample_sequence(idx)
+        data = self._sample_to_data(sample)
         torch_data = dict_apply(data, torch.from_numpy)
         return torch_data
--- diffusion_policy/env_runner/maniskill_runner.py
+++ diffusion_policy/env_runner/maniskill_runner.py
@@ -20,6 +20,90 @@ from diffusion_policy.gym_util.video_recording_wrapper import VideoRecordingWrap
 from diffusion_policy.policy.base_lowdim_policy import BaseLowdimPolicy
 from diffusion_policy.common.pytorch_util import dict_apply
 from diffusion_policy.env_runner.base_lowdim_runner import BaseLowdimRunner
+from diffusion_policy.env_runner.base_image_runner import BaseImageRunner
+
+from mani_skill2.vector.vec_env import VecEnvObservationWrapper
+from mani_skill2.utils.common import flatten_dict_space_keys, flatten_state_dict
+
+class ManiSkillRGBDWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        assert env.obs_mode == "rgbd"
+        self.observation_space = self.init_observation_space(env.observation_space)
+
+    @staticmethod
+    def init_observation_space(obs_space: spaces.Dict):
+        # States include robot proprioception (agent) and task information (extra)
+        # NOTE: SB3 does not support nested observation spaces, so we convert them to flat spaces
+        state_spaces = []
+        state_spaces.extend(flatten_dict_space_keys(obs_space["agent"]).spaces.values())
+        state_spaces.extend(flatten_dict_space_keys(obs_space["extra"]).spaces.values())
+        # Concatenate all the state spaces
+        state_size = sum([space.shape[0] for space in state_spaces])
+        state_space = spaces.Box(-np.inf, np.inf, shape=(state_size,))
+
+        # Concatenate all the image spaces
+        image_shapes = []
+        for cam_uid in obs_space["image"]:
+            cam_space = obs_space["image"][cam_uid]
+            image_shapes.append(cam_space["rgb"].shape)
+            image_shapes.append(cam_space["depth"].shape)
+        image_shapes = np.array(image_shapes)
+        assert np.all(image_shapes[0, :2] == image_shapes[:, :2]), image_shapes
+        h, w = image_shapes[0, :2]
+        c = image_shapes[:, 2].sum(0)
+        rgbd_space = spaces.Box(0, np.inf, shape=(c, h, w))
+
+        # Create the new observation space
+        return spaces.Dict({"rgbd": rgbd_space, "state": state_space})
+
+    @staticmethod
+    def convert_observation(observation):
+        # Process images. RGB is normalized to [0, 1].
+        images = []
+        for cam_uid, cam_obs in observation["image"].items():
+            rgb = cam_obs["rgb"] / 255.0
+            depth = cam_obs["depth"]
+
+            # NOTE: SB3 does not support GPU tensors, so we transfer them to CPU.
+            # For other RL frameworks that natively support GPU tensors, this step is not necessary.
+            #if isinstance(rgb, torch.Tensor):
+            #    rgb = rgb.to(device="cpu", non_blocking=True)
+            #if isinstance(depth, torch.Tensor):
+            #    depth = depth.to(device="cpu", non_blocking=True)
+
+            images.append(rgb)
+            images.append(depth)
+
+        # Concatenate all the images
+        rgbd = np.einsum('ijk->kij', np.concatenate(images, axis=-1))
+
+        # Concatenate all the states
+        state = np.hstack(
+            [
+                flatten_state_dict(observation["agent"]),
+                flatten_state_dict(observation["extra"]),
+            ]
+        )
+
+        return dict(rgbd=rgbd, state=state)
+
+    def observation(self, observation):
+        return self.convert_observation(observation)
+
+
+# We separately define an VecEnv observation wrapper for the ManiSkill VecEnv
+# as the gpu optimization makes it incompatible with the SB3 wrapper
+class ManiSkillRGBDVecEnvWrapper(VecEnvObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        assert env.obs_mode == "rgbd"
+        self.observation_space = ManiSkillRGBDWrapper.init_observation_space(
+            env.observation_space
+        )
+
+    def observation(self, observation):
+        return ManiSkillRGBDWrapper.convert_observation(observation)
 
 
 class ManiskillStateRunner(BaseLowdimRunner):
@@ -28,7 +112,6 @@ class ManiskillStateRunner(BaseLowdimRunner):
                  output_dir,
                  env_id,
                  control_mode,
-                 observation_mode="state",
                  n_train=10,
                  n_train_vis=3,
                  train_start_seed=0,
@@ -41,7 +124,6 @@ class ManiskillStateRunner(BaseLowdimRunner):
                  n_latency_steps=0,
                  fps=10,
                  crf=22,
-                 past_action=False,
                  tqdm_interval_sec=5.0,
                  n_envs=None):
         super().__init__(output_dir)
@@ -60,7 +142,7 @@ class ManiskillStateRunner(BaseLowdimRunner):
             import mani_skill2.envs
             env = gym.make(
                 env_id,
-                obs_mode=observation_mode,
+                obs_mode='state',
                 control_mode=control_mode,
             )
             return MultiStepWrapper(VideoRecordingWrapper(
@@ -153,7 +235,6 @@ class ManiskillStateRunner(BaseLowdimRunner):
         self.n_obs_steps = n_obs_steps
         self.n_action_steps = n_action_steps
         self.n_latency_steps = n_latency_steps
-        self.past_action = past_action
         self.max_steps = max_steps
         self.tqdm_interval_sec = tqdm_interval_sec
 
@@ -191,7 +272,6 @@ class ManiskillStateRunner(BaseLowdimRunner):
 
             # start rollout
             obs = env.reset()
-            past_action = None
             policy.reset()
 
             pbar = tqdm.tqdm(
@@ -203,10 +283,6 @@ class ManiskillStateRunner(BaseLowdimRunner):
             while not done:
                 # create obs dict
                 np_obs_dict = {'obs': obs.astype(np.float32)}
-                if self.past_action and (past_action is not None):
-                    # TODO: not tested
-                    np_obs_dict['past_action'] = past_action[:, -(
-                        self.n_obs_steps - 1):].astype(np.float32)
 
                 # device transfer
                 obs_dict = dict_apply(
@@ -226,7 +302,235 @@ class ManiskillStateRunner(BaseLowdimRunner):
                 # step env
                 obs, reward, done, info = env.step(action)
                 done = np.all(done)
-                past_action = action
+
+                # update pbar
+                pbar.update(action.shape[1])
+            pbar.close()
+
+            # collect data for this round
+            all_video_paths[this_global_slice] = env.render()[this_local_slice]
+            all_rewards[this_global_slice] = env.call(
+                'get_attr', 'reward')[this_local_slice]
+        # import pdb; pdb.set_trace()
+
+        # log
+        max_rewards = collections.defaultdict(list)
+        log_data = dict()
+        for i in range(len(self.env_fns)):
+            seed = self.env_seeds[i]
+            prefix = self.env_prefixs[i]
+            max_reward = np.max(all_rewards[i])
+            max_rewards[prefix].append(max_reward)
+            log_data[prefix + f'sim_max_reward_{seed}'] = max_reward
+
+            # visualize sim
+            video_path = all_video_paths[i]
+            if video_path is not None:
+                sim_video = wandb.Video(video_path)
+                log_data[prefix + f'sim_video_{seed}'] = sim_video
+
+        # log aggregate metrics
+        for prefix, value in max_rewards.items():
+            name = prefix + 'mean_score'
+            value = np.mean(value)
+            log_data[name] = value
+
+        return log_data
+
+
+class ManiskillRGBDRunner(BaseImageRunner):
+
+    def __init__(self,
+                 output_dir,
+                 env_id,
+                 control_mode,
+                 n_train=10,
+                 n_train_vis=3,
+                 train_start_seed=0,
+                 n_test=22,
+                 n_test_vis=6,
+                 test_start_seed=10000,
+                 max_steps=200,
+                 n_obs_steps=8,
+                 n_action_steps=8,
+                 fps=10,
+                 crf=22,
+                 tqdm_interval_sec=5.0,
+                 n_envs=None):
+        super().__init__(output_dir)
+
+        if n_envs is None:
+            n_envs = n_train + n_test
+
+        # handle latency step
+        # to mimic latency, we request n_latency_steps additional steps
+        steps_per_render = max(10 // fps, 1)
+
+        # assert n_obs_steps <= n_action_steps
+        def env_fn():
+            import mani_skill2.envs
+            env = gym.make(
+                env_id,
+                obs_mode='rgbd',
+                control_mode=control_mode,
+            )
+            env = ManiSkillRGBDWrapper(env)
+            return MultiStepWrapper(VideoRecordingWrapper(
+                env,
+                video_recoder=VideoRecorder.create_h264(fps=fps,
+                                                        codec='h264',
+                                                        input_pix_fmt='rgb24',
+                                                        crf=crf,
+                                                        thread_type='FRAME',
+                                                        thread_count=1),
+                file_path=None,
+                steps_per_render=steps_per_render),
+                                    n_obs_steps=n_obs_steps,
+                                    n_action_steps=n_action_steps,
+                                    max_episode_steps=max_steps)
+
+        env_fns = [env_fn] * n_envs
+        env_seeds = list()
+        env_prefixs = list()
+        env_init_fn_dills = list()
+        # train
+        for i in range(n_train):
+            seed = train_start_seed + i
+            enable_render = i < n_train_vis
+
+            def init_fn(env, seed=seed, enable_render=enable_render):
+                # setup rendering
+                # video_wrapper
+                assert isinstance(env.env, VideoRecordingWrapper)
+                env.env.video_recoder.stop()
+                env.env.file_path = None
+                if enable_render:
+                    filename = pathlib.Path(output_dir).joinpath(
+                        'media',
+                        wv.util.generate_id() + ".mp4")
+                    filename.parent.mkdir(parents=False, exist_ok=True)
+                    filename = str(filename)
+                    env.env.file_path = filename
+
+                # set seed
+                assert isinstance(env, MultiStepWrapper)
+                env.seed(seed)
+
+            env_seeds.append(seed)
+            env_prefixs.append('train/')
+            env_init_fn_dills.append(dill.dumps(init_fn))
+
+        # test
+        for i in range(n_test):
+            seed = test_start_seed + i
+            enable_render = i < n_test_vis
+
+            def init_fn(env, seed=seed, enable_render=enable_render):
+                # setup rendering
+                # video_wrapper
+                assert isinstance(env.env, VideoRecordingWrapper)
+                env.env.video_recoder.stop()
+                env.env.file_path = None
+                if enable_render:
+                    filename = pathlib.Path(output_dir).joinpath(
+                        'media',
+                        wv.util.generate_id() + ".mp4")
+                    filename.parent.mkdir(parents=False, exist_ok=True)
+                    filename = str(filename)
+                    env.env.file_path = filename
+
+                # set seed
+                assert isinstance(env, MultiStepWrapper)
+                env.seed(seed)
+
+            env_seeds.append(seed)
+            env_prefixs.append('test/')
+            env_init_fn_dills.append(dill.dumps(init_fn))
+
+        env = SyncVectorEnv(env_fns)
+
+        # test env
+        # env.reset(seed=env_seeds)
+        # x = env.step(env.action_space.sample())
+        # imgs = env.call('render')
+        # import pdb; pdb.set_trace()
+
+        self.env = env
+        self.env_fns = env_fns
+        self.env_seeds = env_seeds
+        self.env_prefixs = env_prefixs
+        self.env_init_fn_dills = env_init_fn_dills
+        self.fps = fps
+        self.crf = crf
+        self.n_obs_steps = n_obs_steps
+        self.n_action_steps = n_action_steps
+        self.max_steps = max_steps
+        self.tqdm_interval_sec = tqdm_interval_sec
+
+    def run(self, policy: BaseImageRunner):
+        device = policy.device
+        dtype = policy.dtype
+
+        env = self.env
+
+        # plan for rollout
+        n_envs = len(self.env_fns)
+        n_inits = len(self.env_init_fn_dills)
+        n_chunks = math.ceil(n_inits / n_envs)
+
+        # allocate data
+        all_video_paths = [None] * n_inits
+        all_rewards = [None] * n_inits
+
+        for chunk_idx in range(n_chunks):
+            start = chunk_idx * n_envs
+            end = min(n_inits, start + n_envs)
+            this_global_slice = slice(start, end)
+            this_n_active_envs = end - start
+            this_local_slice = slice(0, this_n_active_envs)
+
+            this_init_fns = self.env_init_fn_dills[this_global_slice]
+            n_diff = n_envs - len(this_init_fns)
+            if n_diff > 0:
+                this_init_fns.extend([self.env_init_fn_dills[0]] * n_diff)
+            assert len(this_init_fns) == n_envs
+
+            # init envs
+            env.call_each('run_dill_function',
+                          args_list=[(x, ) for x in this_init_fns])
+
+            # start rollout
+            obs = env.reset()
+            policy.reset()
+
+            pbar = tqdm.tqdm(
+                total=self.max_steps,
+                desc=f"Eval ManiskillRunner {chunk_idx+1}/{n_chunks}",
+                leave=False,
+                mininterval=self.tqdm_interval_sec)
+            done = False
+            while not done:
+                # create obs dict
+                np_obs_dict = dict(obs)
+
+                # device transfer
+                obs_dict = dict_apply(
+                    np_obs_dict,
+                    lambda x: torch.from_numpy(x).to(device=device))
+
+                # run policy
+                with torch.no_grad():
+                    action_dict = policy.predict_action(obs_dict)
+
+                # device_transfer
+                np_action_dict = dict_apply(
+                    action_dict, lambda x: x.detach().to('cpu').numpy())
+
+                action = np_action_dict['action']
+
+                # step env
+                obs, reward, done, info = env.step(action)
+                done = np.all(done)
 
                 # update pbar
                 pbar.update(action.shape[1])
